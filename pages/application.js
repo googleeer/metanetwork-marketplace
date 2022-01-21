28f0755ff3490e6d80568bb8213942d943e909b7
googleeer <vyacheslav.bobok@gmail.com>
2022-01-21 23:39:30 +0700

    Neftify => Metanetwork

--- pages/application.js
+++ pages/application.js
@@ -2,30 +2,30 @@ import Preferences from "../components/form/Preferences";
 import LayoutAdmin from "../components/layout/LayoutAdmin";
 import SettingsMenu from "../components/layout/SettingsMenu";
 function SettingsPreferences() {
-    return (
-        <>
-            <LayoutAdmin
-                headTitle="Application"
-                pageTitle="Application"
-                pageTitleSub={"Neftify Settings Application page"}
-                pageClass={"admin"}
-                parent={"Settings"}
-                child={"Application"}
-            >
-                <SettingsMenu />
+  return (
+    <>
+      <LayoutAdmin
+        headTitle="Application"
+        pageTitle="Application"
+        pageTitleSub={"Metanetwork Settings Application page"}
+        pageClass={"admin"}
+        parent={"Settings"}
+        child={"Application"}
+      >
+        <SettingsMenu />
 
-                <div className="row">
-                    <div className="col-xxl-12">
-                        <h4 className="card-title mb-3">Preperences</h4>
-                        <div className="card">
-                            <div className="card-body">
-                                <Preferences />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </LayoutAdmin>
-        </>
-    );
+        <div className="row">
+          <div className="col-xxl-12">
+            <h4 className="card-title mb-3">Preperences</h4>
+            <div className="card">
+              <div className="card-body">
+                <Preferences />
+              </div>
+            </div>
+          </div>
+        </div>
+      </LayoutAdmin>
+    </>
+  );
 }
 export default SettingsPreferences;
--- pages/settings.js
+++ pages/settings.js
@@ -5,45 +5,45 @@ import UpdateAvatar from "../components/form/UpdateAvatar";
 import SettingsMenu from "../components/layout/SettingsMenu";
 
 function SettingsProfile() {
-    return (
-        <>
-            <LayoutAdmin
-                headTitle="Profile"
-                pageTitle="Profile"
-                pageTitleSub={"Neftify Profile page"}
-                pageClass={"admin"}
-                parent={"Settings"}
-                child={"Profile"}
-            >
-                <SettingsMenu />
-                <div className="row">
-                    <div className="col-xxl-6 col-xl-6 col-lg-6">
-                                <h4 className="card-title mb-3">User Profile</h4>
-                        <div className="card">
-                            <div className="card-body">
-                                <UpdateAvatar />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xxl-6 col-xl-6 col-lg-6">
-                                <h4 className="card-title mb-3">Update Profile</h4>
-                        <div className="card">
-                            <div className="card-body">
-                                <UpdateInfo />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xxl-12">
-                                <h4 className="card-title mb-3">Personal Information</h4>
-                        <div className="card">
-                            <div className="card-body">
-                                <PersonalInfo />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </LayoutAdmin>
-        </>
-    );
+  return (
+    <>
+      <LayoutAdmin
+        headTitle="Profile"
+        pageTitle="Profile"
+        pageTitleSub={"Metanetwork Profile page"}
+        pageClass={"admin"}
+        parent={"Settings"}
+        child={"Profile"}
+      >
+        <SettingsMenu />
+        <div className="row">
+          <div className="col-xxl-6 col-xl-6 col-lg-6">
+            <h4 className="card-title mb-3">User Profile</h4>
+            <div className="card">
+              <div className="card-body">
+                <UpdateAvatar />
+              </div>
+            </div>
+          </div>
+          <div className="col-xxl-6 col-xl-6 col-lg-6">
+            <h4 className="card-title mb-3">Update Profile</h4>
+            <div className="card">
+              <div className="card-body">
+                <UpdateInfo />
+              </div>
+            </div>
+          </div>
+          <div className="col-xxl-12">
+            <h4 className="card-title mb-3">Personal Information</h4>
+            <div className="card">
+              <div className="card-body">
+                <PersonalInfo />
+              </div>
+            </div>
+          </div>
+        </div>
+      </LayoutAdmin>
+    </>
+  );
 }
 export default SettingsProfile;
